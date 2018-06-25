5bbbb799e3f0df07fdcd53d206e34926bcb6c667
Unknown <adhivira@live.com>
2018-06-25 18:33:13 +0200

    asd

--- localStorage.js
+++ localStorage.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     console.log("STARTME---" + new Date().getTime())
-    for (var i = 0; i < 75000; i++) {
+    for (var i = 0; i < 175000; i++) {
         localStorage.setItem(i, "");
         // console.log(localStorage.getItem(i));
     }
